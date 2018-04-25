1aa54146fbb3f2092dd4b5cd8a03771e3c66abce
Princeton Evans <princetonevans@yahoo.com>
2018-04-25 14:37:14 -0500

    hard coded data showing up

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Document } from './document';
+import { Document } from './document';
 
 @Component({
   selector: 'app-documents',
@@ -14,6 +14,20 @@ export class DocumentsComponent implements OnInit {
         file_url: 'http://google.com',
         updated_at: '4/25/18',
         image_url: 'http://google.com',
+      },
+      {
+        title: 'My second Doc',
+        description: 'proposals',
+        file_url: 'http://google.com',
+        updated_at: '4/26/18',
+        image_url: 'http://googleplus.com',
+      },
+      {
+        title: 'My third Doc',
+        description: 'proposals',
+        file_url: 'http://google.com',
+        updated_at: '4/28/18',
+        image_url: 'http://googleanalytics.com',
       }
     ];
   constructor() { }
