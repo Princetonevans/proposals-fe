523e4a5c3ea5cb31d804ce421dda8d42482ab2d2
Princeton Evans <princetonevans@yahoo.com>
2018-04-26 18:38:03 -0500

    added proposal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal-list/proposal-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    DocumentsComponent
+    DocumentsComponent,
+    ProposalListComponent
   ],
   imports: [
     BrowserModule,
