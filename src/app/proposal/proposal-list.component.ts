f012606dfd8df9f0532114a0b085ad3b1de6929c
Princeton Evans <princetonevans@yahoo.com>
2018-04-26 20:09:43 -0500

    added hard coded data

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Proposal } from './proposal';
 @Component ({
     moduleId: module.id,
     selector: 'proposal-list',
     templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+    proposalOne: Proposal = new Proposal(5, 'Princeton Creative Design', 'http://www.princetonevans.com', 'Angular 2', 150, 120, 15, 'Princetonevans@yahoo.com')
+    proposalTwo: Proposal = new Proposal(5, 'Princeton Creative Digital', 'http://www.princetonevans.com', 'Angularjs', 140, 110, 13, 'Princetonevans@gmail.com')
+    proposalThree: Proposal = new Proposal(5, 'Princeton Creative ', 'http://www.princetonevans.com', 'Angular 4', 130, 100, 10, 'Princetonevans@office.com')
+
+    proposals: Proposal[] = [
+        this.proposalOne,
+        this.proposalTwo,
+        this.proposalThree
+    ]
+}
