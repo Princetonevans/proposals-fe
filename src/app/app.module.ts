fd7a42045ba244b402eb43705105f66975be7bfd
Princeton Evans <princetonevans@yahoo.com>
2018-04-29 08:55:09 -0500

    added a proposal show route and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ProposalNewComponent } from './proposal/proposal-new.component';
     HomepageComponent,
     DocumentsComponent,
     ProposalListComponent,
-    ProposalNewComponent
+    ProposalNewComponent,
+    ProposalShowComponent
   ],
   imports: [
     BrowserModule,
